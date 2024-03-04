769842d2e249b7ca42f1c412d14900314fdedc3c
Brian Ogungbe <99332476+B00132889@users.noreply.github.com>
2024-03-04 22:03:16 +0000

    Added Navbar & Cart

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 
 Route::get('products/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
 
+Route::get('products/additem/{id}', 'App\Http\Controllers\productController@additem')->name('products.additem');
 
 //Route::resource('products', App\Http\Controllers\productController::class);
 
